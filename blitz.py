c176baf5614b9746846fa21a529fd739e7c8df61
josh goldsmith <josh@spectrum.horse>
2020-07-05 12:44:13 +0100

    first draft of program

--- blitz.py
+++ blitz.py
@@ -1,8 +1,16 @@
 import random
 import sys
 
-attack_num = int(sys.argv[1])
-defend_num = int(sys.argv[2])
+MAX_ATTACK = 3
+MAX_DEFEND = 2
+
+attack_start = int(sys.argv[1])
+defend_start = int(sys.argv[2])
+
+attack_num = attack_start
+defend_num = defend_start
+
+num_rolls = 0
 
 def roll(num):
     dice = []
@@ -13,7 +21,34 @@ def roll(num):
 
     return dice
 
-a = roll(attack_num)
-d = roll(defend_num)
-print(a)
-print(d)
+def battle(attack_roll, defend_roll, attack_num, defend_num):
+
+    for i in range(len(defend_roll)):
+        if (len(attack_roll) > 0) and (len(attack_roll) > 0):
+            if attack_roll.pop() > defend_roll.pop():
+                defend_num -= 1
+            else:
+                attack_num -= 1
+
+    return (attack_num, defend_num)
+
+while(attack_num > 0 and defend_num > 0):
+    if (attack_num >= MAX_ATTACK) and (defend_num >= MAX_DEFEND):
+        attack_num, defend_num = battle(roll(MAX_ATTACK), roll(MAX_DEFEND), attack_num, defend_num)
+
+    elif (attack_num >= MAX_ATTACK) and (defend_num < MAX_DEFEND):
+        attack_num, defend_num = battle(roll(MAX_ATTACK), roll(defend_num), attack_num, defend_num)
+
+    elif (attack_num < MAX_ATTACK) and (defend_num >= MAX_DEFEND):
+        attack_num, defend_num = battle(roll(attack_num), roll(MAX_DEFEND), attack_num, defend_num)
+
+    else:
+        attack_num, defend_num = battle(roll(attack_num), roll(defend_num), attack_num, defend_num)
+    num_rolls += 1
+
+if attack_num > defend_num:
+    print("Attack wins after %d rolls, with %d troop(s) remaining" % (num_rolls, attack_num))
+else:
+    print("Defend wins after %d rolls, with %d troops remaining" % (num_rolls, defend_num))
+
+print("Casualties:\n Attack: %d\n Defend: %d" % (attack_start-attack_num, defend_start-defend_num))
